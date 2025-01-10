5bb2532187fd9d494be18b19819180b214458dbe
mohamedtarek45 <moka76615@gmail.com>
2025-01-10 14:21:37 +0200

    Update

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -5,6 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__dirname);
 const MulterSetting = multer({
   limits: 900000,
   storage: multer.diskStorage({
