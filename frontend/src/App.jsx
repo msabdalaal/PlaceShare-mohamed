38ee844885f9e23999fafb3dbe0228b51ba21e87
mohamedtarek45 <moka76615@gmail.com>
2025-01-12 05:11:41 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,88 +1,79 @@
-import AppContent from "./AppContent";
-import { queryClient } from "./shared/util/http";
-import { QueryClientProvider } from "@tanstack/react-query";
+import React, { Suspense } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import Root from "./Root";
+import { AuthShared } from "./shared/context/auth-context";
+import { sendToken } from "./shared/util/http";
+import { useQuery } from "@tanstack/react-query";
+
+// Lazy load components
+const Users = React.lazy(() => import("./users/pages/User"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlace"));
+const Auth = React.lazy(() => import("./users/pages/Auth"));
+const UpdatePlace = React.lazy(() => import("./places/pages/updatePlace"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+
+// Import loader for Users
+import { loader as usersLoader } from "./users/pages/User";
+import LoadingSpinner from "./shared/components/UiElement/LoadingSpinner";
+
 function App() {
+  const AuthData = AuthShared();
+  const { data, isLoading } = useQuery({
+    queryFn: sendToken,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (data?.token) {
+    AuthData.login({ id: data.token.id });
+  }
+
+  let children = [];
+  if (AuthData.isLoggedIn) {
+    children = [
+      { path: "places/:placeId", element: <UpdatePlace /> },
+      { path: "places/new", element: <NewPlace /> },
+      { path: ":UserId/Places", element: <UserPlaces /> },
+    ];
+  } else {
+    children = [
+      { path: "auth", element: <Auth /> },
+      { path: ":UserId/Places", element: <UserPlaces /> },
+    ];
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: <Users />, loader: usersLoader },
+        ...children,
+      ],
+    },
+    { path: "*", element: <Navigate to="/" replace /> },
+  ]);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppContent />
-    </QueryClientProvider>
+    <Suspense
+      fallback={
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <RouterProvider router={router} />
+    </Suspense>
   );
 }
-
 export default App;
-
-
-
-// import React, { Suspense,  } from "react";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Navigate,
-// } from "react-router-dom";
-// import Root from "./Root";
-// import { AuthShared } from "./shared/context/auth-context";
-// import { queryClient, sendToken } from "./shared/util/http";
-// import { QueryClientProvider, useQuery } from "@tanstack/react-query";
-
-// // Lazy load components
-// const Users = React.lazy(() => import("./users/pages/User"));
-// const UserPlaces = React.lazy(() => import("./places/pages/UserPlace"));
-// const Auth = React.lazy(() => import("./users/pages/Auth"));
-// const UpdatePlace = React.lazy(() => import("./places/pages/updatePlace"));
-// const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
-
-// // Import loader for Users
-// import { loader as usersLoader } from "./users/pages/User";
-// import LoadingSpinner from "./shared/components/UiElement/LoadingSpinner";
-
-// function App() {
-//   const AuthData = AuthShared();
-//   const { data, isLoading } = useQuery({
-//     queryFn:sendToken
-//   })
-//   if(data.token){
-//     AuthData.login({id:data.token.id})
-//   }
-//   if(isLoading){
-//     return <LoadingSpinner />
-//   }
-//   let children = [];
-//   if (AuthData.isLoggedIn) {
-//     children = [
-//       { path: "places/:placeId", element: <UpdatePlace /> },
-//       { path: "places/new", element: <NewPlace /> },
-//       { path: ":UserId/Places", element: <UserPlaces /> },
-//     ];
-//   } else {
-//     children = [
-//       { path: "auth", element: <Auth /> },
-//       { path: ":UserId/Places", element: <UserPlaces /> },
-//     ];
-//   }
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <Root />,
-//       children: [
-//         { index: true, element: <Users />, loader: usersLoader },
-//         ...children,
-//       ],
-//     },
-//     { path: "*", element: <Navigate to="/" replace /> },
-//   ]);
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <Suspense
-//         fallback={
-//           <div className="center">
-//             <LoadingSpinner />
-//           </div>
-//         }
-//       >
-//         <RouterProvider router={router} />;
-//       </Suspense>
-//     </QueryClientProvider>
-//   );
-// }
-
-// export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import { AuthContextProvider } from "./shared/context/auth-context.jsx";
 import "./index.css";
 import App from "./App.jsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./shared/util/http.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
