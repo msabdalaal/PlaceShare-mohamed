7e6d4f398b01861360fe57946eab843b3ce34362
mohamedtarek45 <moka76615@gmail.com>
2025-01-12 07:03:08 +0200

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./shared/util/http.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
     </QueryClientProvider>
-  </StrictMode>
+
 );
